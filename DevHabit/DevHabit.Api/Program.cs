4a786b029584cc51adbaebf810f770a208171a2b
Ricardo Zapanta Jr. <ricardo.zapanta@caplinq.com>
2026-01-05 18:41:56 +0800

    Refactor service registration into extension methods
    Move service registration from Program.cs to DependencyInjection.cs using extension methods. This modularizes configuration for controllers, error handling, database, observability, and app services, resulting in cleaner and more maintainable startup code. No functional changes were made.

--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,69 +1,14 @@
-using DevHabit.Api.Database;
-using DevHabit.Api.Dtos.Habits;
-using DevHabit.Api.Entities;
+using DevHabit.Api;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middleware;
-using DevHabit.Api.Services.Sorting;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Npgsql;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-})
-    .AddNewtonsoftJson()
-    .AddXmlDataContractSerializerFormatters();
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options
-    .UseNpgsql(
-        builder.Configuration.GetConnectionString("Database"),
-        npgsqlOptions => npgsqlOptions
-            .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application)));
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-    .WithTracing(tracing => tracing
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddNpgsql())
-    .WithMetrics(metrics => metrics
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddRuntimeInstrumentation())
-    .UseOtlpExporter();
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeScopes = true;
-    options.IncludeFormattedMessage = true;
-});
-
-builder.Services.AddTransient<SortMappingProvider>();
-builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
-    HabitMapper.SortMapping);
+builder
+    .AddControllers()
+    .AddErrorHandling()
+    .AddDatabase()
+    .AddObservability()
+    .AddApplicationServices();
 
 var app = builder.Build();
 
