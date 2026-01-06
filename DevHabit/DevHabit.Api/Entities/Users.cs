c7f522d037620e1925d44d8ba32da5b78d923491
reekzap <ricardozapanta.rz@gmail.com>
2026-01-06 11:17:44 +0800

    Add Users entity class to represent application users
    Introduced Users.cs with the Users entity in the DevHabit.Api.Entities namespace. This class includes properties for user identification, email, name, creation and update timestamps, and an identity provider ID, with documentation explaining the purpose of the IdetityId field.

---
+++ DevHabit/DevHabit.Api/Entities/Users.cs
@@ -0,0 +1,16 @@
+﻿namespace DevHabit.Api.Entities;
+
+public sealed class Users
+{
+    public string Id { get; set; } = null!;
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    /// <summary>
+    /// Store the IdentityId from the Identity provider.
+    /// This cound be any identity provider like Azure AD B2C, Auth0, Firebase Auth, etc.
+    /// </summary>
+    public string IdetityId { get; set; } = string.Empty;
+}
